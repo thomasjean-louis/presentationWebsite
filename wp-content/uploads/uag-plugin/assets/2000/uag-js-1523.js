56bd5c305040ea09756c74dfe1c85894f1f1e60c
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-10-28 14:31:47 +0100

    selfhost wordpress

--- wp-content/uploads/uag-plugin/assets/2000/uag-js-1523.js
+++ wp-content/uploads/uag-plugin/assets/2000/uag-js-1523.js
@@ -58,30 +58,13 @@ document.addEventListener("DOMContentLoaded", function(){ 				window.addEventLis
 						}
 					} );
 				} );
-							window.addEventListener( 'DOMContentLoaded', () => {
-					const blockScope = document.querySelector( '.uagb-block-9cc9c575' );
-					if ( ! blockScope ) {
-						return;
-					}
-
-					const anchorElement = blockScope.querySelector('a');
-					if (!anchorElement) {
-						return;
-					} 
-
-					 
-					blockScope.addEventListener('keydown', (event) => {
-						if ( 13 === event.keyCode || 32 === event.keyCode ) {
-							event.preventDefault();
-							 
-							anchorElement.click();	
-						}
-					} );
-				} );
 			window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-df72e80f', {"layout":"number","heading":"$\/month","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":0.5,"totalNumber":120,"decimalPlaces":1,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-df72e80f', {"layout":"number","heading":"$\/month","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":0.5,"totalNumber":120,"decimalPlaces":1,"animationDuration":1000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-70698ffa', {"layout":"number","heading":"hours of work","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":4,"totalNumber":120,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-70698ffa', {"layout":"number","heading":"hours availability","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":24,"totalNumber":120,"decimalPlaces":0,"animationDuration":1000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-3b325ad0', {"layout":"number","heading":"environments","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":2,"totalNumber":120,"decimalPlaces":0,"animationDuration":1000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
  });
