551ce3352571959a7deb9dc3544758fda26b24fd
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-11-15 15:27:37 +0100

    firqt commit self-host wordpress

--- wp-content/uploads/uag-plugin/assets/0/uag-js-10.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"8599dc1c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":false,"afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-8599dc1c', 10 );
-});
- });
