8604c593451db3ddf5d6816e3cdb325e19a8e375
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-11-23 16:54:13 +0100

    update projects, home

--- wp-content/uploads/uag-plugin/assets/2000/uag-js-1523.js
+++ wp-content/uploads/uag-plugin/assets/2000/uag-js-1523.js
@@ -19,6 +19,26 @@ document.addEventListener("DOMContentLoaded", function(){ 				window.addEventLis
 					} );
 				} );
 							window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-9ca60c84' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+							window.addEventListener( 'DOMContentLoaded', () => {
 					const blockScope = document.querySelector( '.uagb-block-c9714a37' );
 					if ( ! blockScope ) {
 						return;
