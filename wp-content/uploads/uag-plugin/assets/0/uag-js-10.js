f4b82da1cc4b60e76a4ddf07d0b765e75d9ee8a5
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-11-24 11:51:11 +0100

    fix broken plugin extract

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-10.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"8599dc1c","reCaptchaEnable":true,"reCaptchaType":"v3","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-8599dc1c', 10 );
+});
+ });
